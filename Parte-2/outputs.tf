aa2d9deeb8bba19e23a65795c97c3fdab52c1672
Jonas <147929152+jonasbraganca@users.noreply.github.com>
2024-07-13 22:41:47 -0300

    Delete Parte-2 directory

--- Parte-2/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "instance_id" {
-    description = "ID da instancia EC2"
-    value = aws_instance.example.id
-}
-
-output "public_ip" {
-    description = "IP publico da instancia EC2"
-    value = aws.instance.example.public_ip 
-}
